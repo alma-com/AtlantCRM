97a7b418f91ee6603d1cb0f64fc175ff7e1a9c8d
zualex32 <Azubarev32@yandex.ru>
2015-12-28 16:08:47 +0300

    Подключен AdminLTE

--- config/app.php
+++ config/app.php
@@ -155,6 +155,11 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		/*
+		* Extra
+		*/
+		Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -201,6 +206,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+		
+		'Form' => Collective\Html\FormFacade::class,
+		'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
