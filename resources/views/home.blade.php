f1a4cb995e2469647d50d7bb332eac48f3681dbd
zualex32 <Azubarev32@yandex.ru>
2015-12-29 15:31:02 +0300

    Header прописан для главной

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content-header')
-<h1>Dashboard <small>Control panel</small></h1>
+<h1>Атлант<small>Система поддержки сайтов</small></h1>
 <ol class="breadcrumb">
 	<li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
 </ol>
