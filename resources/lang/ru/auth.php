ffbca24dce0cf405775f5d9a910f49e128bd9947
zualex32 <Azubarev32@yandex.ru>
2015-12-31 12:05:54 +0300

    Руссификация

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Учетная запись не существует.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте еще раз через :seconds секунд.',
 
 ];
