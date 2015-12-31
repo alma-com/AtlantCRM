1a6815466101936e6f8c73f527c6aa91afe7dbc0
zualex32 <Azubarev32@yandex.ru>
2015-12-31 10:42:47 +0300

    Добавлена ссылка на добавление пользователя

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -12,56 +12,67 @@
 @section('content')
 
 
-<div class="box">
-	<div class="box-body">
-		@if(count($users) > 0)
+<div class="row">
+	<div class="col-md-12">
+		<div class="box">
+			<div class="box-body">
+				
+				<div class="btn-group margin-bottom">
+					<a href="{{ route('users.create') }}" class="btn btn-success">Добавить пользователя</a>
+				</div>
 			
-
-
-			@if(count($users) > 25) 
-				<table class="data-table table table-bordered table-striped"  data-order='[[ 1, "asc" ]]'>
-			@else
-				<table class="data-table-small table table-bordered table-striped" data-order='[[ 1, "asc" ]]'>
-			@endif
-				<thead>
-					<tr>
-						<th class="no-sort">
-							<input type="checkbox" class="checkbox-toggle" name="checkbox-toggle" data-name="user[]" value="">
-						</th>
-						<th>ФИО</th>
-						<th>E-mail</th>
-						<th>Роль</th>
-					</tr>
-				</thead>
-				<tbody>
-					@foreach($users as $user)
-						<tr>
-							<td><input type="checkbox" name="user[]" value="{{ $user->id }}"></td>
-							<td><a href="{{ route('users.edit', $user->id) }}">{{ $user->name }}</a></td>
-							<td>{{ $user->email }}</td>
-							<td></td>
-						</tr>
-					@endforeach
-				</tbody>
-				<tfoot>
-					<tr>
-						<td colspan="4">							
-							<div class="box-body no-padding">
-								<div class="table-controls">
+				@if(count($users) > 0)
+					@if(count($users) > 25) 
+						<table class="data-table table table-bordered table-striped"  data-order='[[ 1, "asc" ]]'>
+					@else
+						<table class="data-table-small table table-bordered table-striped" data-order='[[ 1, "asc" ]]'>
+					@endif
+						<thead>
+							<tr>
+								<th class="no-sort">
 									<input type="checkbox" class="checkbox-toggle" name="checkbox-toggle" data-name="user[]" value="">
-									<div class="btn-group">
-										<button type="button" class="btn btn-default btn-sm"><i class="fa fa-pencil"></i></button>
-										<button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+								</th>
+								<th>ФИО</th>
+								<th>E-mail</th>
+								<th>Роль</th>
+								<th>Организация</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($users as $user)
+								<tr>
+									<td><input type="checkbox" name="user[]" value="{{ $user->id }}"></td>
+									<td><a href="{{ route('users.edit', $user->id) }}">{{ $user->name }}</a></td>
+									<td>{{ $user->email }}</td>
+									<td></td>
+									<td></td>
+								</tr>
+							@endforeach
+						</tbody>
+						<tfoot>
+							<tr>
+								<td colspan="5">							
+									<div class="box-body no-padding">
+										<div class="table-controls">
+											<input type="checkbox" class="checkbox-toggle" name="checkbox-toggle" data-name="user[]" value="">
+											<div class="btn-group">
+												<button type="button" class="btn btn-default btn-sm"><i class="fa fa-pencil"></i></button>
+												<button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
-						</td>
-					</tr>
-				</tfoot>
-			</table>
-			
-		@endif
+								</td>
+							</tr>
+						</tfoot>
+					</table>
+					
+				@endif
+			</div>
+		</div>
 	</div>
+	
+	
+
 </div>
 
 
