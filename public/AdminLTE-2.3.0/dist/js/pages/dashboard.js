632c2a44d10a1fd4044aa2a5c249d7319d7d34a6
zualex32 <Azubarev32@yandex.ru>
2016-01-07 14:21:25 +0300

    Ошибка в js

--- public/AdminLTE-2.3.0/dist/js/pages/dashboard.js
+++ public/AdminLTE-2.3.0/dist/js/pages/dashboard.js
@@ -124,7 +124,7 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
-  var area = new Morris.Area({
+  /*var area = new Morris.Area({
     element: 'revenue-chart',
     resize: true,
     data: [
@@ -193,7 +193,7 @@ $(function () {
     area.redraw();
     donut.redraw();
     line.redraw();
-  });
+  });*/
 
   /* The todo list plugin */
   $(".todo-list").todolist({
