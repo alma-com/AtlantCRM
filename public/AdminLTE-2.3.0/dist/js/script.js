e5901b8457458e26fac0d561f701d260e137db5f
zualex32 <Azubarev32@yandex.ru>
2016-01-19 19:31:55 +0300

    Подгрузка стилей после ajax

--- public/AdminLTE-2.3.0/dist/js/script.js
+++ public/AdminLTE-2.3.0/dist/js/script.js
@@ -1,8 +1,18 @@
+$(document).ready(function() {
+	initAjaxClass();
+	initiCheck();
+	initiCheckboxToggle();
+	initDataTable();
+});
+
+
+
 $.ajaxSetup({
     headers: {
         'X-XSRF-TOKEN': $('input[name="csrf-token"]').val()
     }
 });
+
 
 
 /*
@@ -13,39 +23,11 @@ $(document).ajaxStart(function() {
 });
 
 
-$('.ajax').click(function(event){
-	event.preventDefault();
-	
-	var url = $(this).attr('href') || $(this).attr('data-url');		//URL
-	var method = $(this).attr('data-method') || 'GET';			//Метод POST или GET
-	var content = $(this).attr('data-content') || 'ajax-content';		//Контейнер в который вставится результат
-
-	if(url && method && content){
-		$.ajax({
-			url: url,
-			type: method,
-			data: {
-				'_token' : $('meta[name="csrf-token"]').attr('content')
-			},
-			success: function(result){
-				$('.'+content).html(result);
-			}
-		});
-	}else{
-		if(!url){alert = 'Нет url';}
-		if(!method){alert = 'Нет method';}
-		if(!content){alert = 'Нет content';}
-	}
-});
-
-
 
 /*
-* AJAX click
+* Ajax загрузка страницы по url
 */
-function setPage(page, popstate){
-	$wrap = $("#ajax-content");
-	
+function setPage(page, popstate){	
 	$.get(page, function(data){
 		var title = document.title;
 		var content = '';
@@ -57,8 +39,13 @@ function setPage(page, popstate){
 
 		
 		document.title = title;
-		$wrap.html(content);
+		$("#ajax-content").html(content);
 		$('.content-header').html(content_header);
+		
+		initAjaxClass();
+		initiCheck();
+		initiCheckboxToggle();
+		initDataTable();
 		
 		if(!popstate){
 			history.pushState({page: page, type: "page"}, title, page);
@@ -82,56 +69,94 @@ if (history.pushState) {
 
 
 
+
+/*
+* initAjaxClass клик на ссылку с классом ajax
+*/
+function initAjaxClass(){
+	$('.ajax').click(function(event){
+		event.preventDefault();
+		
+		var url = $(this).attr('href') || $(this).attr('data-url');		//URL
+		var method = $(this).attr('data-method') || 'GET';			//Метод POST или GET
+		var content = $(this).attr('data-content') || 'ajax-content';		//Контейнер в который вставится результат
+
+		if(url && method && content){
+			$.ajax({
+				url: url,
+				type: method,
+				data: {
+					'_token' : $('meta[name="csrf-token"]').attr('content')
+				},
+				success: function(result){
+					$('.'+content).html(result);
+				}
+			});
+		}else{
+			if(!url){alert = 'Нет url';}
+			if(!method){alert = 'Нет method';}
+			if(!content){alert = 'Нет content';}
+		}
+	});
+}
+
+
+
 /*
 * iCheck
 */
-$('input[type="checkbox"]').iCheck({
-	checkboxClass: 'icheckbox_flat-blue',
-	radioClass: 'iradio_flat-blue'
-});
-
+function initiCheck(){
+	$('input[type="checkbox"]').iCheck({
+		checkboxClass: 'icheckbox_flat-blue',
+		radioClass: 'iradio_flat-blue'
+	});
+}
 
 
 /*
 * checkbox-toggle
 */
-$(".checkbox-toggle").on('ifToggled', function () {
-	var clicks = $(this).data('clicks');
-	var name = $(this).data('name');
-	if (clicks) {
-		//Uncheck all checkboxes
-		$("input[name='"+name+"']").iCheck("uncheck");
-		$(".checkbox-toggle[data-name='"+name+"']").iCheck("uncheck");
-	} else {
-		//Check all checkboxes
-		$("input[name='"+name+"']").iCheck("check");
-		$(".checkbox-toggle[data-name='"+name+"']").iCheck("check");
-	}
-	$(this).data("clicks", !clicks);
-});
-
+function initiCheckboxToggle(){
+	$(".checkbox-toggle").on('ifToggled', function () {
+		var clicks = $(this).data('clicks');
+		var name = $(this).data('name');
+		if (clicks) {
+			//Uncheck all checkboxes
+			$("input[name='"+name+"']").iCheck("uncheck");
+			$(".checkbox-toggle[data-name='"+name+"']").iCheck("uncheck");
+		} else {
+			//Check all checkboxes
+			$("input[name='"+name+"']").iCheck("check");
+			$(".checkbox-toggle[data-name='"+name+"']").iCheck("check");
+		}
+		$(this).data("clicks", !clicks);
+	});
+}
 
 
 /*
 *	DataTable
 */
-$(".data-table").DataTable({
-	"language": {"url": "//cdn.datatables.net/plug-ins/1.10.10/i18n/Russian.json"},
-	"pageLength": 25,
-	"columnDefs": [{ targets: 'no-sort', orderable: false, width: "20px" }],
-	"lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "Все"]],
-	stateSave: true
-});
+function initDataTable(){
+	$(".data-table").DataTable({
+		"language": {"url": "//cdn.datatables.net/plug-ins/1.10.10/i18n/Russian.json"},
+		"pageLength": 25,
+		"columnDefs": [{ targets: 'no-sort', orderable: false, width: "20px" }],
+		"lengthMenu": [[25, 50, 100, -1], [25, 50, 100, "Все"]],
+		stateSave: true
+	});
 
-$(".data-table-small").DataTable({
-	"language": {"url": "//cdn.datatables.net/plug-ins/1.10.10/i18n/Russian.json"},
-	"pageLength": 25,
-	"paging": false,
-	"lengthChange": false,
-	"searching": false,
-	"ordering": true,
-	"info": false,
-	"autoWidth": false,
-	"columnDefs": [{ targets: 'no-sort', orderable: false, width: "20px" }],
-	stateSave: true
-});
+	$(".data-table-small").DataTable({
+		"language": {"url": "//cdn.datatables.net/plug-ins/1.10.10/i18n/Russian.json"},
+		"pageLength": 25,
+		"paging": false,
+		"lengthChange": false,
+		"searching": false,
+		"ordering": true,
+		"info": false,
+		"autoWidth": false,
+		"columnDefs": [{ targets: 'no-sort', orderable: false, width: "20px" }],
+		stateSave: true
+	});
+}
+
