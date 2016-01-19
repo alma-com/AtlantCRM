da8c6ff9ef4dcb5672fc3ec730e3c81c6e8329d2
zualex32 <Azubarev32@yandex.ru>
2016-01-19 08:59:45 +0300

    Настроен редирект после восстановления пароля

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -19,7 +19,9 @@ class PasswordController extends Controller
     */
 
     use ResetsPasswords;
-
+	
+	protected $redirectTo = '/';
+	
     /**
      * Create a new password controller instance.
      *
