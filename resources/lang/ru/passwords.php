f6ec807ea180a5a0a3d869b6d617ff10fc55dfd5
zualex32 <Azubarev32@yandex.ru>
2016-01-19 10:28:53 +0300

    Руссифицированы формы сброса пароля

--- resources/lang/ru/passwords.php
+++ resources/lang/ru/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Пароль должен быть не менее шести символов и совпадать с подтверждением.',
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Ссылка на сброс пароля отправлена!',
+    'token' => 'Ошибочный код сброса пароля.',
+    'user' => "Не удалось найти пользователя с указанным электронным адресом.",
 
 ];
