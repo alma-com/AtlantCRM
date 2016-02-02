8db691d388b8bde51276e9fef14fe230a6630632
zualex <Azubarev32@yandex.ru>
2016-02-02 09:59:34 +0300

    Revert "Добавлена ссылка для обновления всех настроек сайта"
    This reverts commit 3a9dc0ea8cca51ff52c94b1666bff96d2e54eba5.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,8 +31,6 @@ Route::group(['middleware' => ['web']], function () {
 		Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 		
 		
-		Route::get('/update-settings', 'SettingController@update');
-		
 		Route::resource('users', 'UserController');
 		Route::post('users/destroyItems', ['as' => 'users.destroyItems', 'uses' => 'UserController@destroyItems']);
 		Route::post('users/updateItems', ['as' => 'users.updateItems', 'uses' => 'UserController@updateItems']);
