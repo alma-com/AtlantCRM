11ca1126a3d1081d873e3985696c06c9a0c3e730
zualex <Azubarev32@yandex.ru>
2016-02-03 15:36:54 +0300

    Добавлены консольные команды для обновление прав и для добавления админа

---
+++ app/Console/Commands/PermissionUpdate.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\PermissionGroup;
+use App\Permission;
+use App\Role;
+use App\User;
+
+class PermissionUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'permission:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		$this->addGroup();
+		$this->addPermGeneral();
+		$this->addPermUser();
+
+		$this->addRole();
+    }
+	
+	
+	
+	/**
+	 * Группы прав
+	 */
+	public function addGroup()
+	{
+		$arrGroup = array(
+			'name' => 'general',
+            'display_name' => 'Общие настройки',
+		);
+		PermissionGroup::addGroup($arrGroup);
+		
+		$arrGroup = array(
+			'name' => 'user',
+            'display_name' => 'Пользователи',
+		);
+		PermissionGroup::addGroup($arrGroup);
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Добавление общих прав
+	 */
+	public function addPermGeneral()
+	{
+		$groupName = 'general';
+		
+		$arrPermission = array(
+			'name' => 'manage_role',
+            'display_name' => 'Управление ролями',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+	}
+	
+	
+	
+	/**
+	 * Добавление прав для пользователей
+	 */
+	public function addPermUser()
+	{
+		$groupName = 'user';
+		
+		$arrPermission = array(
+			'name' => 'show_user',
+            'display_name' => 'Просмотр',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+		
+		$arrPermission = array(
+			'name' => 'add_user',
+            'display_name' => 'Добавление',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+		
+		$arrPermission = array(
+			'name' => 'edit_user',
+            'display_name' => 'Редактирование',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+		
+		$arrPermission = array(
+			'name' => 'change_role_user',
+            'display_name' => 'Смена роли',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+		
+		$arrPermission = array(
+			'name' => 'delete_user',
+            'display_name' => 'Удаление',
+		);
+		Permission::addPermission($arrPermission, $groupName);
+	}
+	
+	
+	
+	/**
+	 * Добавление роли
+	 */
+	public function addRole()
+	{		
+		$arrRole = array(
+			'name' => 'admin',
+            'display_name' => 'Администратор',
+		);
+		$role = Role::addRole($arrRole);
+		
+		$role
+			->assignPermission('manage_role')
+			->assignPermission('show_user')
+			->assignPermission('add_user')
+			->assignPermission('edit_user')
+			->assignPermission('change_role_user')
+			->assignPermission('delete_user');
+	}
+}
---
+++ app/Console/Commands/UserAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\User;
+
+class UserAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:admin {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create admin';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+		$email = $this->argument('email');
+		$password = $this->argument('password');
+		
+		$arrData = array(
+			'email' => $email,
+			'password' => $password,
+		);
+		
+		$user = User::updateData($arrData);
+		$user->assignRole('admin');
+    }
+}
