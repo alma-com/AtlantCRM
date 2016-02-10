c243014674328c1c537a3188ca636a79faa0fab3
zualex <Azubarev32@yandex.ru>
2016-02-10 11:47:27 +0300

    middleware access permission

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,5 +48,7 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+		
+        'access' => \App\Http\Middleware\AccessMiddleware::class,
     ];
 }
