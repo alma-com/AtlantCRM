cf0501a8d1ca288d7d29299a5384a3ef61b78428
zualex <Azubarev32@yandex.ru>
2016-05-13 20:51:50 +0300

    TDD model Task

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,67 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-	//
+	protected $fillable = [
+		'name',
+		'description',
+		'date_start',
+		'deadline',
+		'planned_duration',
+		'project_id',
+    ];
+
+
+	/**
+	 * assign director
+	 */
+	public function assignDirector($director = '')
+	{
+		return $this;
+	}
+
+
+	/**
+	 * delete director
+	 */
+	public function deleteDirector($director = '')
+	{
+		return $this;
+	}
+
+
+	/**
+	 * assign employee
+	 */
+	public function assignEmployee($employee = '')
+	{
+		return $this;
+	}
+
+
+	/**
+	 * delete employee
+	 */
+	public function deleteEmployee($employee = '')
+	{
+		return $this;
+	}
+
+
+	/**
+	 * assign watcher
+	 */
+	public function assignWatchers($watcher = '')
+	{
+		return $this;
+	}
+
+
+	/**
+	 * delete watcher
+	 */
+	public function deleteWatchers($watcher = '')
+	{
+		return $this;
+	}
+
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+
+
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
+    return [
+		'name' => $faker->text($maxNbChars = 200),
+		'description' => $faker->text($maxNbChars = 400),
+		'date_start' => Carbon\Carbon::now(),
+		'deadline' => Carbon\Carbon::now()->addWeeks(2),
+		'planned_duration' => $faker->randomDigitNotNull(),
+    ];
+});
