7692829280df7fe331e2e41c2e5ac6a9a0122b7b
zualex <Azubarev32@yandex.ru>
2016-07-18 14:05:46 +0300

    change response ajax in Request.php

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -3,8 +3,34 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
+use Alma;
+use HTML;
+use Response;
 
 abstract class Request extends FormRequest
 {
-    //
+    /**
+     * Get the proper failed validation response for the request.
+     *
+     * @param  array  $errors
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function response(array $errors)
+    {
+        if ($this->ajax() || $this->wantsJson()) {
+            //return new JsonResponse($errors, 422);
+
+            return Response::json([
+                'errFields' => $errors,
+                'description' => Alma::getDescription('warning', HTML::ul(array_flatten($errors))),
+                'status' => 'warning',
+                'message' => 'Предупреждение',
+            ]);
+        }
+
+        return $this->redirector->to($this->getRedirectUrl())
+            ->withInput($this->except($this->dontFlash))
+            ->withErrors($errors, $this->errorBag);
+    }
 }
