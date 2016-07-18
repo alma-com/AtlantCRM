6f1d7d1f2642bfb2575cd37ffdbb86956f7a7637
zualex <Azubarev32@yandex.ru>
2016-07-18 14:06:30 +0300

    add UserRequest

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class UserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|email|max:255',
+            'password' => 'required|min:6|max:255',
+            'password_confirmation' => 'required|same:password',
+        ];
+    }
+}
