02e2e40edc19dea5c12680d23027f7be64ae4ce4
zualex <Azubarev32@yandex.ru>
2016-08-01 09:12:51 +0300

    fix command permission:update

--- app/Console/Commands/PermissionUpdate.php
+++ app/Console/Commands/PermissionUpdate.php
@@ -56,19 +56,15 @@ class PermissionUpdate extends Command
      */
     public function addGroup()
     {
-        $arrGroup = array(
+        PermissionGroup::create([
             'name' => 'general',
             'display_name' => 'Общие настройки',
-        );
-        PermissionGroup::add($arrGroup);
+        ]);
 
-        $arrGroup = array(
+        PermissionGroup::create([
             'name' => 'user',
             'display_name' => 'Пользователи',
-        );
-        PermissionGroup::add($arrGroup);
-
-        return true;
+        ]);
     }
 
 
@@ -77,13 +73,13 @@ class PermissionUpdate extends Command
      */
     public function addPermGeneral()
     {
-        $groupName = 'general';
+        $generalGroup = PermissionGroup::where('name', 'general')->first();
 
-        $arrPermission = array(
+        $permission = Permission::create([
             'name' => 'manage_role',
             'display_name' => 'Управление ролями',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $generalGroup->id,
+        ]);
     }
 
 
@@ -93,37 +89,37 @@ class PermissionUpdate extends Command
      */
     public function addPermUser()
     {
-        $groupName = 'user';
+        $userGroup = PermissionGroup::where('name', 'user')->first();
 
-        $arrPermission = array(
+        $permShow = Permission::create([
             'name' => 'show_user',
             'display_name' => 'Просмотр',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $userGroup->id,
+        ]);
 
-        $arrPermission = array(
+        $permAdd = Permission::create([
             'name' => 'add_user',
             'display_name' => 'Добавление',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $userGroup->id,
+        ]);
 
-        $arrPermission = array(
+        $permEdit = Permission::create([
             'name' => 'edit_user',
             'display_name' => 'Редактирование',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $userGroup->id,
+        ]);
 
-        $arrPermission = array(
+        $permRole = Permission::create([
             'name' => 'change_role_user',
             'display_name' => 'Смена роли',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $userGroup->id,
+        ]);
 
-        $arrPermission = array(
+        $permDelete = Permission::create([
             'name' => 'delete_user',
             'display_name' => 'Удаление',
-        );
-        Permission::add($arrPermission, $groupName);
+            'group_id' => $userGroup->id,
+        ]);
     }
 
 
@@ -133,18 +129,25 @@ class PermissionUpdate extends Command
      */
     public function addRole()
     {
-        $arrRole = array(
+        $role = Role::create([
             'name' => 'admin',
             'display_name' => 'Администратор',
-        );
-        $role = Role::add($arrRole);
+        ]);
 
-        $role
-            ->assignPermission('manage_role')
-            ->assignPermission('show_user')
-            ->assignPermission('add_user')
-            ->assignPermission('edit_user')
-            ->assignPermission('change_role_user')
-            ->assignPermission('delete_user');
+        $permManage = Permission::where('name', 'manage_role')->first();
+        $permShow = Permission::where('name', 'show_user')->first();
+        $permAdd = Permission::where('name', 'add_user')->first();
+        $permEdit = Permission::where('name', 'edit_user')->first();
+        $permRole = Permission::where('name', 'change_role_user')->first();
+        $permDelete = Permission::where('name', 'delete_user')->first();
+
+        $role->permissions()->sync([
+            $permManage->id,
+            $permShow->id,
+            $permAdd->id,
+            $permEdit->id,
+            $permRole->id,
+            $permDelete->id,
+        ]);
     }
 }
